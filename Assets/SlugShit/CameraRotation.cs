68a495502598f26dfb90eebb1a340ce73a286a24
sniegs <manguzia@uni.coventry.ac.uk>
2016-02-14 14:39:03 +0000

    Slug model and movement

---
+++ Assets/SlugShit/CameraRotation.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRotation : MonoBehaviour {
+
+
+	public Rigidbody rb;
+	public float rotationSpeed = 4f;
+	// Use this for initialization
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		rb.rotation = Quaternion.Euler (rb.rotation.eulerAngles + new Vector3
+			(0f, rotationSpeed * Input.GetAxis ("Mouse X"), 0f));
+	}
+}
---
+++ Assets/SlugShit/SlugMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlugMovement : MonoBehaviour {
+
+	public Rigidbody rb;
+	public float speed;
+	public float rotationSpeed = 1f;
+
+	void Start() 
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+	void Update() 
+	{
+		if (Input.GetKey(KeyCode.W))
+		{
+			rb.velocity = transform.right * speed;
+		}
+		if (Input.GetKey(KeyCode.S))
+		{
+			rb.velocity = -transform.right * speed;;
+		}
+	}
+}
