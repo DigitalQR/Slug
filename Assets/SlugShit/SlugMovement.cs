ca6367932d9a6d233840a0b85bfe3eb918236a47
Sam Potter <sam_pot_97@hotmail.com>
2016-03-09 09:57:24 +0000

    Changed LEAP controls

--- Assets/SlugShit/SlugMovement.cs
+++ Assets/SlugShit/SlugMovement.cs
@@ -2,41 +2,40 @@
 using Leap;
 using System.Collections;
 
-public class SlugMovement : MonoBehaviour {
+public class SlugMovement : MonoBehaviour
+{
 
-	Rigidbody rb;
+    Rigidbody rb;
     public Transform LEAP_controller;
-	public float speed;
+    public float speed;
 
-	void Start() 
-	{
-		rb = GetComponent<Rigidbody>();
-	}
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
-	void FixedUpdate() 
-	{
+    void FixedUpdate()
+    {
         Controller controller = new Controller();
         HandList hands = controller.Frame().Hands;
-        
+
 
         if (hands.Count != 0)
         {
-            Hand hand = hands[0];
-            Vector3 hand_direction = new Vector3(-hand.PalmPosition.ToUnity().x, hand.PalmPosition.ToUnity().z, hand.PalmPosition.ToUnity().y);
-            hand_direction = Camera.main.transform.rotation * hand_direction;
+            Transform camera_transform = Camera.main.transform;
 
-            Vector3 hand_position = LEAP_controller.position + hand_direction;
-            
+            Vector3 direction = camera_transform.forward;
+            if (hands.Count == 2)
+            {
+                direction *= 0;
+            }
 
-
-            Vector3 direction = hand_position - transform.position;
             direction.y = 0;
-            Debug.DrawRay(transform.position, direction, Color.red);
             direction.Normalize();
             direction.y = rb.velocity.y / speed;
 
             rb.velocity = direction * speed;
-            
+
         }
-	}
+    }
 }
