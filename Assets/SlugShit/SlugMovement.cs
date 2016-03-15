2e051a1b1287f539a6b5c74cb5969096030ec044
thoma211 <perfectdark_0@hotmail.co.uk>
2016-03-15 15:33:17 +0000

    Possible Leap Swipe Movement

--- Assets/SlugShit/SlugMovement.cs
+++ Assets/SlugShit/SlugMovement.cs
@@ -8,16 +8,37 @@ public class SlugMovement : MonoBehaviour
     Rigidbody rb;
     public Transform LEAP_controller;
     public float speed;
+    Controller controller;
+    GestureList gestures;
 
     void Start()
     {
+        controller = new Controller();
         rb = GetComponent<Rigidbody>();
+
+        controller.EnableGesture(Gesture.GestureType.TYPESWIPE);
+        controller.Config.SetFloat("Gesture.Swipe.MinLength", 150.0f);
+        controller.Config.SetFloat("Gesture.Swipe.MinVelocity", 1000f);
+        controller.Config.Save();
     }
 
     void FixedUpdate()
     {
-        Controller controller = new Controller();
+       
         HandList hands = controller.Frame().Hands;
+
+        Frame frame = controller.Frame();
+        gestures = frame.Gestures();
+
+        if (gestures[0].Type == Gesture.GestureType.TYPE_SWIPE)
+        {
+            SwipeGesture swipe = new SwipeGesture(gestures[0]);
+            Vector swipeDirection = swipe.Direction;
+            if(swipeDirection == Vector.Left)
+                transform.Translate(new Vector3(-1,0,0));
+            else if(swipeDirection == Vector.Right)
+                transform.Translate(new Vector3(1, 0, 0));
+        }
 
 
         if (hands.Count != 0)
