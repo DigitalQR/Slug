7380541193f77160b0c92ad630b2ff193b7c048f
thoma211 <perfectdark_0@hotmail.co.uk>
2016-03-30 15:50:54 +0100

    Gumball machine stuff: See below:

---
+++ Assets/Script/Scene 2/Gumball Machine/GumballMachineCheckPoint.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GumballMachineCheckPoint : MonoBehaviour {
+
+    public int TotalGumballs;
+    public GameObject Gumball;
+    private int CurrentGumballCount = 0;
+    public Animator GumballMachineAnimator;
+
+    void OnTriggerEnter(Collider Object)
+    {
+        if (Object.tag.Equals("Player") && CurrentGumballCount < TotalGumballs)
+        {
+            Instantiate(Gumball);
+            GumballMachineAnimator.SetTrigger("GumballAnimation");
+            CurrentGumballCount++;
+        }
+    }
+
+
+}
---
+++ Assets/Script/SpawnOnCreate.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnOnCreate : MonoBehaviour {
+
+    public GameObject spawnObject;
+
+	// Use this for initialization
+	void Start () {
+        Instantiate(spawnObject, transform.position,Quaternion.identity);
+	
+	}
+	
+
+}
